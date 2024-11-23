7ab2a6afb2409f24d046623dda11367c27f26b14
wobuhuiliaomei <1049591822@qq.com>
2024-11-23 16:43:30 +0800

    建立项目

---
+++ src/main/java/com/lyf/kob/KobApplication.java
@@ -0,0 +1,13 @@
+package com.lyf.kob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KobApplication.class, args);
+    }
+
+}
